52ed72d5a1e771f1d9892107b48b0d023f0647ea
Kamary <kamarygillespie@Kamarys-MBP.lan>
2022-09-09 13:03:45 -0600

    added server and middleware

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const express = require("express");
 const path = require("path");
 const fs = require('fs');
 const util = require('util');
+
+// Async Processes
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// server
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// middleware
+app.use(express.static("/public"));
